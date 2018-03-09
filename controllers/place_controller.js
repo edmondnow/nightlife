4475a272ea901a78f09c5fca20b479e3b9e678a2
edmondnow <edmond.bitay@gmail.com>
2018-03-09 19:09:46 +0100

    added routes and controllers molds

---
+++ controllers/place_controller.js
@@ -0,0 +1,32 @@
+var yelpGet = require('yelp');
+var User = require('../models/user.js');
+var Place = require('../models/place.js');
+
+//stores data from yelp API based on latest search
+var latestSearch;
+
+
+exports.index_get = function(req, res){
+ res.send('NOT IMPLEMENTED')
+}
+
+exports.search_post = function(req, res){
+ res.send('NOT IMPLEMENTED')
+}
+
+
+exports.rsvp_post = function(req, res){
+ res.send('NOT IMPLEMENTED')
+}
+
+exports.register_post = function(req, res){
+ res.send('NOT IMPLEMENTED')
+}
+
+exports.login_post = function(req, res){
+ res.send('NOT IMPLEMENTED')
+}
+
+exports.logout_post = function(req, res){
+ res.send('NOT IMPLEMENTED')
+}
---
+++ routes/place.js
@@ -0,0 +1,26 @@
+var router = require('express').Router();
+var place_controller = require('../controllers/place_controller.js')
+
+
+
+
+
+//get main page with default view
+router.get('/', place_controller.index_get);
+//get main page after search
+router.post('/', place_controller.search_post);
+//post rsvp data and return pug view with updated rsvp
+router.post('/rsvp', place_controller.rsvp_post);
+
+//redirect too logged in page
+router.post('/register', place_controller.register_post); 
+//redirect to logged in page
+router.post('/login', place_controller.login_post);
+
+//redirect to logged out page
+router.post('/login', place_controller.logout_post);
+
+//yelpGet('Tilburg').then(data => console.log(data));
+
+module.exports = router;
+
