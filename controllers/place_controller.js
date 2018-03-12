deab1231d624979e927796e6810c8c5a4cfeae87
edmondnow <edmond.bitay@gmail.com>
2018-03-12 21:45:02 +0100

    added modals"

--- controllers/place_controller.js
+++ controllers/place_controller.js
@@ -4,22 +4,19 @@ var Place = require('../models/place.js');
 var async = require('async');
 
 
-
-//stores data from yelp API based on latest search
-var latestSearch;
-
-
-
 exports.index_get = function(req, res){
  res.render('index', {title: 'Places'});
 }
+
+
 
 exports.search_post = function(req, res){
   yelp(req.body.location).then(function(response){
     Place.collection.insert(response, function(){
       Place.find({location: req.body.location}).exec(function(err, results){
-        if(err) next(err)
+        if(err) console.log(err)
         console.log('ey');
+        latestSearch = req.body.location;
         res.render('index', {title: 'Places', data: results});
        })
      })
@@ -27,8 +24,22 @@ exports.search_post = function(req, res){
 }
 
 
-exports.rsvp_post = function(req, res){
- res.send('NOT IMPLEMENTED')
+exports.rsvp_yes = function(req, res){
+  Place.findOneAndUpdate({_id: req.body._id}, { $push: { users: "5aa2b4733fa807079042614d" }}).exec(function(err, results){
+    if (err) console.log(err);
+    Place.find({location: req.body.location}).exec(function(err, results){
+      res.render('index', {title: 'Places', data: results});
+    })
+  })
+}
+
+exports.rsvp_no = function(req, res){
+  Place.findOneAndUpdate({_id: req.body._id}, { $pull: { users: "5aa2b4733fa807079042614d" }}).exec(function(err, results){
+    if (err) console.log(err);
+    Place.find({location: req.body.location}).exec(function(err, results){
+      res.render('index', {title: 'Places', data: results});
+    })
+  })
 }
 
 exports.register_post = function(req, res){
--- routes/place.js
+++ routes/place.js
@@ -5,10 +5,14 @@ var place_controller = require('../controllers/place_controller.js')
 
 //get main page with default view
 router.get('/', place_controller.index_get);
+
 //get main page after search
 router.post('/', place_controller.search_post);
+
 //post rsvp data and return pug view with updated rsvp
-router.post('/rsvp', place_controller.rsvp_post);
+router.post('/rsvp_no', place_controller.rsvp_no);
+
+router.post('/rsvp_yes', place_controller.rsvp_yes);
 
 //redirect too logged in page
 router.post('/register', place_controller.register_post); 
@@ -16,7 +20,7 @@ router.post('/register', place_controller.register_post);
 router.post('/login', place_controller.login_post);
 
 //redirect to logged out page
-router.post('/login', place_controller.logout_post);
+router.post('/logout', place_controller.logout_post);
 
 //yelpGet('Tilburg').then(data => console.log(data));
 
