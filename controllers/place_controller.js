9306a7bb55940b33e810757b69a4771457f44fec
edmondnow <edmond.bitay@gmail.com>
2018-03-13 18:15:29 +0100

    commit before changing rsvp functionality

--- controllers/place_controller.js
+++ controllers/place_controller.js
@@ -19,18 +19,46 @@ exports.index_get = function(req, res){
 
 
 exports.search_post = function(req, res){
+	//return data from the yelp api with query string
 	yelp(req.body.location).then(function(response){
-		Place.collection.insert(response, function(){
-			Place.find({location: req.body.location}).exec(function(err, results){
-				if(err) console.log(err)
-				console.log('ey');
-				latestSearch = req.body.location;
-				res.render('index', {title: 'Places', data: results, error: false});
-			 })
-		 })
+		//save query string as latest search cookie
+		req.session.latest = req.body.location;
+		//find if data is already presend in the database
+		Place.find({location: req.body.location}).exec(function(err, locationres){
+			if(err) console.log(err)
+			//if data is not present add response to database
+			if(locationres==null||JSON.stringify(locationres)=='[]'){
+				Place.collection.insert(response, function(err, dbres){
+					if(err) return console.log(err);
+					//if there is no data to be displayed search for UserId
+					if(locationres==null||JSON.stringify(dbres)=='[]'){
+						if(req.session.userId){
+							User.findById({_id: req.session.userId}).exec(function(err, userData){
+								res.render('index', {title: 'Places', data: [{name: 'No results for this location'}], username: userData.username,
+								error: false});
+							})
+						} else {
+							res.render('index', {title: 'Places', data: dbRes, error: false})
+						}
+					}
+
+				})
+			//if data is present in the database display the data
+			} else {
+				if(req.session.userId){
+					//if the user has a session display user information
+					User.findById({_id: req.session.userId}).exec(function(err, userData){
+							res.render('index', {title: 'Places', data: locationres, username: userData.username,
+							error: false});
+						})
+				//if the user has no session display user information
+				} else {
+					res.render('index', {title: 'Places', data: locationres, error: false});
+				}		
+			}
+		})
 	})
 }
-
 
 exports.rsvp_yes = function(req, res){
 	Place.findOneAndUpdate({_id: req.body._id}, { $push: { users: "5aa2b4733fa807079042614d" }}).exec(function(err, results){
@@ -58,7 +86,7 @@ exports.register_post = function(req, res){
 		return next(err);
 	}
 
- 	if (req.body.email &&
+	if (req.body.email &&
 		req.body.username &&
 		req.body.password &&
 		req.body.passwordConf) {
@@ -75,9 +103,15 @@ exports.register_post = function(req, res){
 				return console.log(err);
 			} else {
 				req.session.userId = user._id;
-				User.findById({_id: req.session.userId}).exec(function(err, results){
+				User.findById({_id: req.session.userId}).exec(function(err, userData){
 					if(err) console.log(err)
-					res.render('index', {title: 'Places', session: true, username: results.username, error: false});
+					if(req.session.latest){
+						Place.find({location: req.session.latest}).exec(function(err, placeData){
+							res.render('index', {title: 'Places', session: true, data: placeData, username: userData.username, error: false});
+						})
+					} else {
+							res.render('index', {title: 'Places', session: true, username: userData.username, error: false});
+					}
 				})
 			}
 		});
@@ -86,7 +120,7 @@ exports.register_post = function(req, res){
 
 exports.login_post = function(req, res){
  
- 	if (req.body.email &&
+	if (req.body.email &&
 		req.body.password) {
 		//use schema.create to insert data into the db
 		User.authenticate(req.body.email, req.body.password, function(err, user){
@@ -96,11 +130,16 @@ exports.login_post = function(req, res){
 				res.render('index', {title: 'Places', session: false, error: {type: 'login', error: err}});
 			} else {
 				req.session.userId = user._id;
-				User.findById({_id: req.session.userId}).exec(function(err, results){
+				User.findById({_id: req.session.userId}).exec(function(err, userData){
 					if(err) console.log(err)
-					res.render('index', {title: 'Places', session: true, username: results.username, error: false});
+					if(req.session.latest){
+						Place.find({location: req.session.latest}).exec(function(err, placeData){
+							res.render('index', {title: 'Places', session: true, data: placeData, username: userData.username, error: false});
+						})
+					} else {
+							res.render('index', {title: 'Places', session: true, username: userData.username, error: false});
+					}
 				})
-				
 			}
 		})
 	}
@@ -108,15 +147,15 @@ exports.login_post = function(req, res){
 
 exports.logout_post = function(req, res){
  // GET /logout
-  if (req.session) {
-    // delete session object
-    req.session.destroy(function(err) {
-      if(err) {
-        return next(err);
-      } else {
-        return res.render('index', {title: 'Places'});
-      }
-    });
-  }
+	if (req.session) {
+		// delete session object
+		req.session.destroy(function(err) {
+			if(err) {
+				return next(err);
+			} else {
+				return res.render('index', {title: 'Places'});
+			}
+		});
+	}
 
 }
